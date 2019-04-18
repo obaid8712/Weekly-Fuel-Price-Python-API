c04b33cbe8b198972603a335a1f93a792a945cf7
unknown <obaid8712@gmail.com>
2019-04-18 15:10:52 -0400

    Test DOne

--- diesel_price.py
+++ diesel_price.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[18]:
 
 
 # Dependencies
 import pandas as pd
+import numpy as np
 import requests
 import json
 
@@ -19,40 +20,62 @@ sid='PET.EMD_EPD2D_PTE_R10_DPG.W'
 query_url = url + "api_key=" + api_key + "&series_id=" + sid
 
 
-# In[2]:
+# In[19]:
 
 
 # Run a request to endpoint and convert result to json
 Fuel_data = requests.get(query_url).json()
 
 # Print the json
-#print(json.dumps(Fuel_data, indent=4, sort_keys=True))
+# print(json.dumps(Fuel_data, indent=4, sort_keys=True))
 
 
-# In[3]:
+# In[20]:
+
+
+# PADD 1B CENTRAL ATLANTIC
+sid='PET.EMD_EPD2D_PTE_R1Y_DPG.W'
+Padd1b_url = url + "api_key=" + api_key + "&series_id=" + sid
+
+
+# In[21]:
+
+
+# Run a request to endpoint and convert result to json
+padd1b_data = requests.get(Padd1b_url).json()
+
+# Print the json
+# print(json.dumps(padd1b_data, indent=4, sort_keys=True))
+
+
+# In[22]:
 
 
 day=[]
-rate=[]
+rate1=[]
+rate1b=[]
 day.append(Fuel_data["series"][0]["data"][0][0])
-rate.append(Fuel_data["series"][0]["data"][0][1])
+rate1.append(Fuel_data["series"][0]["data"][0][1])
+rate1b.append(padd1b_data["series"][0]["data"][0][1])
 print(Fuel_data["series"][0]["data"][0][0])
 print(Fuel_data["series"][0]["data"][0][1])
+print(padd1b_data["series"][0]["data"][0][1])
 
 
-# In[4]:
+# In[23]:
 
 
 d={
     "Date":day,
-    "Price":rate
+    "PADD_1":rate1,
+    "PADD_1B":rate1b
 }
 rate_df=pd.DataFrame(data=d)
-#rate_df.head()
+rate_df.head()
 
 
-# In[5]:
+# In[24]:
 
 
-rate_df.to_csv('rate.csv',index=False)
+rate_df.to_csv(f'diesel-rate-{day[0]}.csv',index=False)
 
